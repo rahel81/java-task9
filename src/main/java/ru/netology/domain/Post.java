22ca9483f9a31d55170818d51f0af9cd7c8a8634
Rahel81 <rahimova81@gmail.com>
2021-01-27 11:17:31 +0300

    updated Post.java (added fields from the documentation VK)

--- src/main/java/ru/netology/domain/Post.java
+++ src/main/java/ru/netology/domain/Post.java
@@ -5,9 +5,31 @@ public class Post {
     private int data;
     private int userId;
     private int postId;
-    private String buttonComment;
-    private String buttonLike;
+    private int fromId;
+    private int replyUserId;
+    private int getReplyPostId;
+    private boolean friendsOnly;
+    private String buttonComments;
+    private int canPost;
+    private boolean canClose;
+    private boolean canOpen;
+    private String copyright;
+    private String buttonLikes;
+    private int userLikes;
+    private int canLikes;
+    private int canPublish;
+    private String buttonReposts;
+    private int userReposted;
     private int viewsPost;
+    private String postType;
+    private String postSource;
+    private String geo;
+    private int signerId;
+    private int canPin;
+    private int canDelete;
+    private int canEdit;
+    private int isPinned;
+    private boolean isFavorite;
 
     public String getText() {
         return text;
@@ -41,20 +63,124 @@ public class Post {
         this.postId = postId;
     }
 
-    public String getButtonComment() {
-        return buttonComment;
+    public int getFromId() {
+        return fromId;
     }
 
-    public void setButtonComment(String buttonComment) {
-        this.buttonComment = buttonComment;
+    public void setFromId(int fromId) {
+        this.fromId = fromId;
     }
 
-    public String getButtonLike() {
-        return buttonLike;
+    public int getReplyUserId() {
+        return replyUserId;
     }
 
-    public void setButtonLike(String buttonLike) {
-        this.buttonLike = buttonLike;
+    public void setReplyUserId(int replyUserId) {
+        this.replyUserId = replyUserId;
+    }
+
+    public int getGetReplyPostId() {
+        return getReplyPostId;
+    }
+
+    public void setGetReplyPostId(int getReplyPostId) {
+        this.getReplyPostId = getReplyPostId;
+    }
+
+    public boolean isFriendsOnly() {
+        return friendsOnly;
+    }
+
+    public void setFriendsOnly(boolean friendsOnly) {
+        this.friendsOnly = friendsOnly;
+    }
+
+    public String getButtonComments() {
+        return buttonComments;
+    }
+
+    public void setButtonComments(String buttonComments) {
+        this.buttonComments = buttonComments;
+    }
+
+    public int getCanPost() {
+        return canPost;
+    }
+
+    public void setCanPost(int canPost) {
+        this.canPost = canPost;
+    }
+
+    public boolean isCanClose() {
+        return canClose;
+    }
+
+    public void setCanClose(boolean canClose) {
+        this.canClose = canClose;
+    }
+
+    public boolean isCanOpen() {
+        return canOpen;
+    }
+
+    public void setCanOpen(boolean canOpen) {
+        this.canOpen = canOpen;
+    }
+
+    public String getCopyright() {
+        return copyright;
+    }
+
+    public void setCopyright(String copyright) {
+        this.copyright = copyright;
+    }
+
+    public String getButtonLikes() {
+        return buttonLikes;
+    }
+
+    public void setButtonLikes(String buttonLikes) {
+        this.buttonLikes = buttonLikes;
+    }
+
+    public int getUserLikes() {
+        return userLikes;
+    }
+
+    public void setUserLikes(int userLikes) {
+        this.userLikes = userLikes;
+    }
+
+    public int getCanLikes() {
+        return canLikes;
+    }
+
+    public void setCanLikes(int canLikes) {
+        this.canLikes = canLikes;
+    }
+
+    public int getCanPublish() {
+        return canPublish;
+    }
+
+    public void setCanPublish(int canPublish) {
+        this.canPublish = canPublish;
+    }
+
+    public String getButtonReposts() {
+        return buttonReposts;
+    }
+
+    public void setButtonReposts(String buttonReposts) {
+        this.buttonReposts = buttonReposts;
+    }
+
+    public int getUserReposted() {
+        return userReposted;
+    }
+
+    public void setUserReposted(int userReposted) {
+        this.userReposted = userReposted;
     }
 
     public int getViewsPost() {
@@ -63,5 +189,77 @@ public class Post {
 
     public void setViewsPost(int viewsPost) {
         this.viewsPost = viewsPost;
+    }
+
+    public String getPostType() {
+        return postType;
+    }
+
+    public void setPostType(String postType) {
+        this.postType = postType;
+    }
+
+    public String getPostSource() {
+        return postSource;
+    }
+
+    public void setPostSource(String postSource) {
+        this.postSource = postSource;
+    }
+
+    public String getGeo() {
+        return geo;
+    }
+
+    public void setGeo(String geo) {
+        this.geo = geo;
+    }
+
+    public int getSignerId() {
+        return signerId;
+    }
+
+    public void setSignerId(int signerId) {
+        this.signerId = signerId;
+    }
+
+    public int getCanPin() {
+        return canPin;
+    }
+
+    public void setCanPin(int canPin) {
+        this.canPin = canPin;
+    }
+
+    public int getCanDelete() {
+        return canDelete;
+    }
+
+    public void setCanDelete(int canDelete) {
+        this.canDelete = canDelete;
+    }
+
+    public int getCanEdit() {
+        return canEdit;
+    }
+
+    public void setCanEdit(int canEdit) {
+        this.canEdit = canEdit;
+    }
+
+    public int getIsPinned() {
+        return isPinned;
+    }
+
+    public void setIsPinned(int isPinned) {
+        this.isPinned = isPinned;
+    }
+
+    public boolean isFavorite() {
+        return isFavorite;
+    }
+
+    public void setFavorite(boolean favorite) {
+        isFavorite = favorite;
     }
 }
