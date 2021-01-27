a85b43f225e925556927c1456ec042ea77981ddc
Rahel81 <rahimova81@gmail.com>
2021-01-27 14:45:09 +0300

    added PostManager.java

---
+++ src/main/java/ru/netology/manager/PostManager.java
@@ -0,0 +1,17 @@
+package ru.netology.manager;
+
+
+import ru.netology.domain.Post;
+
+public class PostManage {
+    private Post posts;
+
+    public Post[] search(int userId, String domain, String query, boolean userOnly, int count, int offset) {
+        return null;
+    }
+
+    public void delete(int userId, int postId) {
+    }
+}
+
+
